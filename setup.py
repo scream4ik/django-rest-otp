514052f4221ad87c271611841de7609b3829fb78
Slava <slava.deb@gmail.com>
2018-03-23 13:56:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-rest-otp',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'Django>=2.0,<2.1',
@@ -10,7 +10,7 @@ setup(
         'djangorestframework-jwt<1.12',
         'Pillow',
         'pyotp<2.3',
-        'qrcode<5.4',
+        'qrcode<7.0',
         'redis-collections>=0.4,<0.5'
     ]
 )
