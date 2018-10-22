c7416ab11c166af82137891a46566cc42db0c1db
Slava <slava.deb@gmail.com>
2018-10-22 16:43:30 +0300

    update requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-rest-otp',
-    version='0.1.10',
+    version='0.1.11',
     packages=find_packages(),
     install_requires=[
-        'Django>=2.0,<2.1',
-        'djangorestframework>=3.7,<3.9',
+        'Django>=2.0',
+        'djangorestframework>=3.7',
         'djangorestframework-jwt<1.12',
         'Pillow',
         'pyotp<2.3',
