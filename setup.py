b5cf8659124c8040a347ba48b60938c32621cd5a
Slava <slava.deb@gmail.com>
2020-02-10 18:08:27 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-rest-otp',
-    version='0.1.16',
+    version='0.1.17',
     packages=find_packages(),
     install_requires=[
         'Django>=2.0',
